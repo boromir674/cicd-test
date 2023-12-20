fa5884d6aaeb99805c97c7c0dc86f6a09762c2fa
konstantinos <boromir674@hotmail.com>
2023-12-20 21:06:01 +0200

    feat(code_visualization): add Test Workflow calling Code Visualization and add Python Automated Test

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,10 +16,11 @@ YAML_FILES: t.List[str] = sorted(
 DOCKER_TEST_WORKFLOWS = [x for x in YAML_FILES if 'docker_pol' in x]
 PYPI_TEST_WORKFLOWS = [x for x in YAML_FILES if 'pypi' in x]
 STATIC_CODE_ANALYSIS_WORKFLOWS = [x for x in YAML_FILES if 'static_code' in x]
-
+VISUALIZE_PYTHON_IMPORTS_WORKFLOWS = [x for x in YAML_FILES if 'code_viz' in x]
 
 @pytest.fixture(
-    params=DOCKER_TEST_WORKFLOWS + PYPI_TEST_WORKFLOWS + STATIC_CODE_ANALYSIS_WORKFLOWS
+    params=DOCKER_TEST_WORKFLOWS + PYPI_TEST_WORKFLOWS + \
+        STATIC_CODE_ANALYSIS_WORKFLOWS + VISUALIZE_PYTHON_IMPORTS_WORKFLOWS
 )
 def yaml_workflow(request, github_workflow):
     import yaml
