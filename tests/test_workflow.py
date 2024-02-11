d3dbc5c97f4b2b734bf52ba103a325cb33f41f9e
konstantinos <boromir674@hotmail.com>
2024-02-11 21:42:51 +0200

    test(pypi): automatically map names from runtime Job Matrix to expectations, built from static YAML

--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -30,8 +30,13 @@ def test_workflow_and_its_jobs_are_green(
     ), f"Failed to get workflow runs for Workflow {workflow_id}"
     workflow_runs: t.Dict = workflow_runs_response.json()
 
-    latest_run: int = max(workflow_runs["workflow_runs"], key=lambda x: x["run_number"])
-    print(f"[DEBUG] Previous Workflow Run Number: {latest_run['run_number']}")
+    runs_sequence = list(workflow_runs["workflow_runs"])
+    if len(runs_sequence) == 0:
+        latest_run_nb = 0
+    else:
+        latest_run: int = max(runs_sequence, key=lambda x: x["run_number"])
+        latest_run_nb = latest_run['run_number']
+    print(f"[DEBUG] Previous Workflow Run Number: {latest_run_nb}")
 
     #### TRIGGER WORKFLOW ####
     response = trigger_workflow({'id': workflow_id}, token)
@@ -46,18 +51,23 @@ def test_workflow_and_its_jobs_are_green(
 
     runner_triggered = False
     while not runner_triggered:
+        # HTTP Request
         workflow_runs_response = get_workflow_runs({'id': workflow_id}, token)
         assert (
             workflow_runs_response.status_code == 200
         ), f"Failed to get workflow runs for Workflow {workflow_id}"
         workflow_runs: t.Dict = workflow_runs_response.json()
-        new_latest_run: int = max(
-            workflow_runs["workflow_runs"], key=lambda x: x["run_number"]
-        )
-        runner_triggered = new_latest_run['run_number'] == latest_run['run_number'] + 1
+        new_runs_sequence = list(workflow_runs["workflow_runs"])
+        if len(new_runs_sequence) == 0:
+            new_latest_run = 0
+        else:
+            new_latest_run: int = max(
+                new_runs_sequence, key=lambda x: x["run_number"]
+            )
+        runner_triggered = new_latest_run['run_number'] == latest_run_nb + 1
 
     # THEN Workflow number of runs, increases by 1
-    assert new_latest_run['run_number'] == latest_run['run_number'] + 1
+    assert new_latest_run['run_number'] == latest_run_nb + 1
 
     run_id = new_latest_run.get("id")
     assert run_id, f"Failed to find the workflow run id for {workflow_id}"
@@ -94,7 +104,7 @@ def test_workflow_and_its_jobs_are_green(
     )
     assert (
         jobs_info_response.status_code == 200
-    ), f"Failed to get jobs info for {workflow_run['run_number']}. Reported run_number's: {[i['run_number'] for i in workflow_runs['workflow_runs']]}"
+    ), f"Failed to get jobs info for {workflow_run['run_number']}. Reported run_number's: {[i['run_number'] for i in new_runs_sequence]}"
 
     jobs_info = jobs_info_response.json()
 
@@ -108,6 +118,10 @@ def test_workflow_and_its_jobs_are_green(
         job_key: str = job["name"].split(" / ")[0]
 
         # take care of 'Job Matrix' case, where 1 YAML Job may spawn multiple jobs at runtime
+        # we use a minimal 2-Job Matrix where we convert Runtime Job name
+        # FROM 'build_python (ubuntu-latest, 3.10)' TO build_python
+        job_key = job_key.split(" (")[0]
+
         expected_jobs: t.MutableMapping['str', 'str'] = expectations["jobs"]
         try:
             expected_job_conclusion: str = expected_jobs[job_key]
@@ -118,7 +132,8 @@ def test_workflow_and_its_jobs_are_green(
                 "Expected Jobs: " + '[' + ', '.join(sorted(expected_jobs.keys())) + ']\n\n'
                 "Actual Jobs: " + '[' + ', '.join(sorted([i['name'] for i in jobs_info['jobs']])) + ']'
                 "\n\n"
-                "It might be that one Yaml Job utilizes a Workflow Strategy (aka Job Matrix), which spawns multiple jobs at runtime."
+                "It might be that one Yaml Job utilizes a Workflow Strategy (aka Job Matrix), which spawns multiple jobs at runtime.\n"
+                "In this case, please convert the runtime key to match the one in the Expected Jobs Dict keys.\n"
             )
             raise error
         assert (
