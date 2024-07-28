da30d863231479b6f0f05fba9fa96bfb67c67f4a
konstantinos <boromir674@hotmail.com>
2024-07-28 22:38:42 +0300

    feat: automatically verify that PR closed with Code Merge

--- tests/test_pr_to_release_opened_n_closed.py
+++ tests/test_pr_to_release_opened_n_closed.py
@@ -4,6 +4,7 @@ import pytest
 # Pull Request Model for searching PRs
 # type alias of a key/value with keys 'owner', 'repo', 'pr_number'
 
+
 class PRInfo(t.TypedDict):
     owner: str
     repo: str
@@ -22,24 +23,24 @@ def get_pr_status():
         github_token: str,
     ) -> requests.Response:
 
-
         # DEBUG Code
 
         response = requests.get(
-        "https://api.github.com/repos/{owner}/{repo}/pulls?state=all&base={base_branch}".format(
-            owner=pr_info["owner"],
-            repo=pr_info["repo"],
-            base_branch=pr_info["base_branch"],
-        ),
-        headers={
-            "Authorization": f"Bearer {github_token}",
-        },
-    )
+            "https://api.github.com/repos/{owner}/{repo}/pulls?state=all&base={base_branch}".format(
+                owner=pr_info["owner"],
+                repo=pr_info["repo"],
+                base_branch=pr_info["base_branch"],
+            ),
+            headers={
+                "Authorization": f"Bearer {github_token}",
+            },
+        )
         # find PRs with given base branch
         response.raise_for_status()
 
         print(f"\n[DEBUG] PRs with base branch: {pr_info['base_branch']}")
         from pprint import pprint
+
         pprint(response.json())
         print()
 
@@ -73,17 +74,20 @@ def verify_pr_opened_n_closed(
     get_pr_status,
 ):
     def _verify_pr_opened_n_closed(head_branch: str, base_branch: str, github_token: str):
-    # GIVEN a way to query for the PR status for the relevant github repository
-        def pr_status():
-            return [x['state'] for x in get_pr_status(
-                pr_info={
-                    "owner": "boromir674",
-                    "repo": "gitops-automation",
-                    "head_branch": head_branch,
-                    "base_branch": base_branch,                    
-                },
-                github_token=github_token,
-            )]
+        # GIVEN a way to query for the PR status for the relevant github repository
+        def pr_data():
+            return [
+                x
+                for x in get_pr_status(
+                    pr_info={
+                        "owner": "boromir674",
+                        "repo": "gitops-automation",
+                        "head_branch": head_branch,
+                        "base_branch": base_branch,
+                    },
+                    github_token=github_token,
+                )
+            ]
             # Can be one of: open, closed
 
         # we poll and try to encounter first 'open' and then 'closed' PR
@@ -94,13 +98,16 @@ def verify_pr_opened_n_closed(
 
         # use perf time measure lib
         import time
+
         start_time = time.time()
-        time_limit = 60 # seconds
+        time_limit = 60  # seconds
 
         stop_condition = encountered_closed or (start_time + time_limit < time.time())
         while not stop_condition:
-            states = pr_status()
-            assert len(states) <= 2, f"States: {states}"
+            prs = pr_data()
+            states = [x['state'] for x in prs]
+
+            assert len(prs) <= 2, f"States: {states}"
 
             if 'open' in states:
                 encountered_opened = True
@@ -116,14 +123,29 @@ def verify_pr_opened_n_closed(
 
         # THEN verify PR has opened and closed from User Br to 'release' br
         assert encountered_closed, f"Expecting to observe the PR status closed"
-        assert encountered_opened and encountered_closed, "Expecting to observe the PR status both opened and closed"
+
+        # get pr object with state == closed
+        pr_closed = [x for x in prs if x['state'] == 'closed']
+
+        assert len(pr_closed) == 1, f"Expecting to observe the PR status closed once"
+
+        pr_closed = pr_closed[0]
+
+        # assert PR closed with code merge
+        assert pr_closed['merged_at'] not in {
+            None,
+            '',
+            'None',
+        }, f"Expecting to observe the PR status closed with code merge"
 
     return _verify_pr_opened_n_closed
+
 
 # Requires prior Git Ops event
 @pytest.mark.gitops
 def test_pr_to_release_opened_n_closed(verify_pr_opened_n_closed):
     import os
+
     verify_pr_opened_n_closed(
         head_branch=os.getenv("GO_HEAD_BRANCH"),
         base_branch='release',
