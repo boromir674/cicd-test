7bbdce71ffbd4d4e2a38c9fd988788ad7672b9e8
konstantinos <boromir674@hotmail.com>
2024-07-28 23:02:06 +0300

    feat: called pytest case for checking open PR on main

--- tests/test_release_me_gitops_event_phase_1.py
+++ tests/test_release_me_gitops_event_phase_1.py
@@ -93,7 +93,6 @@ def verify_pr_opened_n_closed(
         # we poll and try to encounter first 'open' and then 'closed' PR
         # we dim success if we only find a 'closed', assuming we were slow to start poll
 
-        encountered_opened = False
         encountered_closed = False
 
         # use perf time measure lib
@@ -141,6 +140,80 @@ def verify_pr_opened_n_closed(
     return _verify_pr_opened_n_closed
 
 
+@pytest.fixture
+def verify_pr_opened():
+    import requests
+
+    def _verify_pr_opened(head_branch: str, base_branch: str, github_token: str):
+        # GIVEN a way to query for the PR status for the relevant github repository
+        # Closure
+        def get_pr_data():
+            response = requests.get(
+                "https://api.github.com/repos/{owner}/{repo}/pulls?state=open&head={head_branch}&base={base_branch}".format(
+                    owner="boromir674",
+                    repo="gitops-automation",
+                    head_branch="{organization}:{branch}".format(
+                        organization='boromir674',
+                        branch=head_branch,
+                    ),
+                    base_branch=base_branch,
+                ),
+                headers={
+                    "Authorization": f"Bearer {github_token}",
+                },
+            )
+            # find PRs with given base branch
+            response.raise_for_status()
+
+            data = response.json()
+            return data
+
+        encountered_opened = False
+
+        import time
+
+        start_time = time.time()
+        time_limit = 60  # seconds
+
+        stop_condition = encountered_opened or (start_time + time_limit < time.time())
+        while not stop_condition:
+            prs = get_pr_data()
+            states = [x['state'] for x in prs]
+
+            assert len(prs) <= 2, f"States: {states}"
+
+            if 'open' in states:
+                encountered_opened = True
+            elif 'closed' in states:
+                encountered_closed = True
+            elif len(states) == 0:
+                pass  # nothing found on remote github server
+            else:
+                raise ValueError(f"Unexpected PR states: {states}")
+
+            stop_condition = encountered_closed or (start_time + time_limit < time.time())
+            time.sleep(3.5)
+
+        # THEN verify PR has opened and closed from User Br to 'release' br
+        assert encountered_closed, f"Expecting to observe the PR status closed"
+
+        # get pr object with state == closed
+        pr_closed = [x for x in prs if x['state'] == 'closed']
+
+        assert len(pr_closed) == 1, f"Expecting to observe the PR status closed once"
+
+        pr_closed = pr_closed[0]
+
+        # assert PR closed with code merge
+        assert pr_closed['merged_at'] not in {
+            None,
+            '',
+            'None',
+        }, f"Expecting to observe the PR status closed with code merge"
+
+    return _verify_pr_opened
+
+
 # Requires prior Git Ops event
 @pytest.mark.gitops
 def test_pr_to_release_opened_n_closed(verify_pr_opened_n_closed):
@@ -149,5 +222,17 @@ def test_pr_to_release_opened_n_closed(verify_pr_opened_n_closed):
     verify_pr_opened_n_closed(
         head_branch=os.getenv("GO_HEAD_BRANCH"),
         base_branch='release',
+        github_token=os.getenv("GITHUB_TOKEN"),
+    )
+
+
+# Requires prior Git Ops event
+@pytest.mark.gitops
+def test_pr_to_main_opened(verify_pr_opened):
+    import os
+
+    verify_pr_opened(
+        head_branch=os.getenv("GO_HEAD_BRANCH"),
+        base_branch='main',
         github_token=os.getenv("GITHUB_TOKEN"),
     )
